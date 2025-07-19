f9b764ee11d37c149aebafd18a5480e3f3ed5ffe
Shreyansh Koladiya <55000207+shreyu1701@users.noreply.github.com>
2025-07-19 14:08:11 -0400

    Fixed: Signin /Signup Screens bottomTabs issue

--- app/src/main/java/com/project/foundoncampus/nav/NavGraph.kt
+++ app/src/main/java/com/project/foundoncampus/nav/NavGraph.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
 import com.project.foundoncampus.views.screens.SignUpScreen
 import com.project.foundoncampus.views.screens.HomeScreen
 import com.project.foundoncampus.views.screens.SearchScreen
@@ -14,19 +15,30 @@ import com.project.foundoncampus.views.screens.ProfileScreen
 import com.project.foundoncampus.views.screens.SignInScreen
 
 @Composable
-fun NavGraph(navController: NavHostController, startDestination: String = Route.Home.routeName) {
+fun NavGraph(navController: NavHostController, startDestination: String = Route.Auth.routeName) {
     NavHost(
-
         navController = navController,
-//        startDestination = Route.SignUp.routeName
         startDestination = startDestination
     ) {
-        composable(Route.SignUp.routeName) { SignUpScreen(navController) }
-        composable(Route.SignIn.routeName) { SignInScreen(navController) }
-        composable(Route.Home.routeName) { HomeScreen(navController) }
-        composable(Route.Search.routeName) { SearchScreen(navController) }
-        composable(Route.Create.routeName) { CreateScreen(navController) }
-        composable(Route.History.routeName) { HistoryScreen(navController) }
-        composable(Route.Profile.routeName) { ProfileScreen(navController) }
+        // Auth Navigation
+        navigation(
+            route = Route.Auth.routeName,
+            startDestination = Route.SignIn.routeName
+        ) {
+            composable(Route.SignIn.routeName) { SignInScreen(navController) }
+            composable(Route.SignUp.routeName) { SignUpScreen(navController) }
+        }
+
+        // Main Navigation
+        navigation(
+            route = Route.Main.routeName,
+            startDestination = Route.Home.routeName
+        ) {
+            composable(Route.Home.routeName) { HomeScreen(navController) }
+            composable(Route.Search.routeName) { SearchScreen(navController) }
+            composable(Route.Create.routeName) { CreateScreen(navController) }
+            composable(Route.History.routeName) { HistoryScreen(navController) }
+            composable(Route.Profile.routeName) { ProfileScreen(navController) }
+        }
     }
 }
--- app/src/main/java/com/project/foundoncampus/nav/Route.kt
+++ app/src/main/java/com/project/foundoncampus/nav/Route.kt
@@ -1,9 +1,15 @@
 package com.project.foundoncampus.nav
 
 sealed class Route(val routeName: String) {
+    // Parent Graphs
+    object Auth : Route("auth")
+    object Main : Route("main")
 
+    // Auth Screens
     object SignUp : Route("sign_up")
     object SignIn : Route("sign_in")
+
+    // Main Screens
     object Home : Route("home")
     object Search : Route("search")
     object Create : Route("create")
